ddefe15ff8266b2825b4972be745dd83ce30805e
Tenderfeel <tenderfeel@gmail.com>
2021-03-15 22:50:07 +0900

    update: partslog

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,40 @@
 <template>
-  <div class="main text-center flex flex-col items-center justify-center">
-    <!-- <logo /> -->
-    <h1 class="title">Nuxt — Tailwind — Netlify CMS</h1>
-    <h2 class="subtitle">Boilerplate</h2>
+  <div class="container mx-auto">
+    <div class="bg-gray-900 shadow overflow-hidden sm:rounded-lg my-4">
+      <div class="px-4 py-5 sm:px-6">
+        <h3 class="text-md leading-6 font-medium text-gray-500">
+          機能紹介
+        </h3>
+      </div>
+      <div class="border-t border-gray-700">
+        <dl class="divide-y divide-gray-800">
+          <div class="bg-gray-900 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-400">
+              絵巻欠片
+            </dt>
+            <dd class="mt-1 text-sm text-gray-500 sm:mt-0 sm:col-span-2">
+              絵巻イベント用計算機。欠片獲得状況の計算と記録ができます。
+            </dd>
+          </div>
+          <div class="bg-gray-900 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-400">
+              火力計算
+            </dt>
+            <dd class="mt-1 text-sm text-gray-500 sm:mt-0 sm:col-span-2">
+              式神の火力計算機です。
+            </dd>
+          </div>
+          <div class="bg-gray-900 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-400">
+              百戦管理
+            </dt>
+            <dd class="mt-1 text-sm text-gray-500 sm:mt-0 sm:col-span-2">
+              異聞ダンジョン百戦アタックモードの攻略管理ツールです。
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
   </div>
 </template>
 
--- pages/partslog/index.vue
+++ pages/partslog/index.vue
@@ -3,7 +3,7 @@
     <h1 class="page-title hidden">絵巻欠片計算機</h1>
 
     <div class="container mx-auto grid sm:grid-cols-2 gap-4 mt-4">
-      <!-- 左 -->
+      <!-- 左:欠片入力 -->
       <div class="shadow overflow-hidden sm:rounded-md order-2 sm:order-1">
         <div class="bg-gray-900 px-3 py-3 sm:p-4">
           <div class="grid grid-cols-6 gap-6">
@@ -50,9 +50,22 @@
           </button>
         </div>
       </div>
-      <!-- 右 -->
+      <!-- 右：合計ポイント -->
       <div class="bg-gray-900 shadow overflow-hidden sm:rounded-md order-1 sm:order-2">
-        <h2 class="leading-6 font-medium text-gray-500 p-2">合計ポイント</h2>
+        <h2 class="leading-6 font-medium text-gray-500 p-2 sm:px-4 border-b border-gray-800">合計ポイント</h2>
+
+        <!-- Total -->
+        <div class="text-lg p-3 sm:px-4 font-bold">
+          0
+        </div>
+
+        <!-- 鯖ごと -->
+        <div class="grid grid-cols-3 gap-2 px-3 sm:px-4">
+          <div class="flex items-center">
+            <span class="block bg-blue-900 text-gray-400 rounded-sm py-0.5 px-1.5 text-xs">1</span>
+            <span class="block px-1 text-blue-600">0</span>
+          </div>
+        </div>
       </div>
     </div>
   </main>
